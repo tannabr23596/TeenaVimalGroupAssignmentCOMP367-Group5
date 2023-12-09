5fdd1b275b13607abe608ee8178fb099f7c3c598
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-12-09 11:10:44 -0500

    first commit

---
+++ src/main/java/com/teena/vimal/devops/TeenaVimalDevopsGroupAssignmentApplication.java
@@ -0,0 +1,15 @@
+package com.teena.vimal.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenaVimalDevopsGroupAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeenaVimalDevopsGroupAssignmentApplication.class, args);
+		System.out.println("Welcome to COMP367 Group Project");
+	
+	}
+
+}
---
+++ src/test/java/com/teena/vimal/devops/TeenaVimalDevopsGroupAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teena.vimal.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenaVimalDevopsGroupAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
