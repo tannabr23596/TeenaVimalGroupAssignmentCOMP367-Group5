7d54406e89a99f707f88b373ac0a2cc8887e3161
tannabr23596 <102817767+tannabr23596@users.noreply.github.com>
2023-12-11 13:57:58 -0500

    Update TeenaVimalDevopsGroupAssignmentApplication.java

--- src/main/java/com/teena/vimal/devops/TeenaVimalDevopsGroupAssignmentApplication.java
+++ src/main/java/com/teena/vimal/devops/TeenaVimalDevopsGroupAssignmentApplication.java
@@ -11,7 +11,7 @@ public class TeenaVimalDevopsGroupAssignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeenaVimalDevopsGroupAssignmentApplication.class, args);
-		System.out.println("Welcome to COMP367 Group Project");
+		System.out.println("Welcome to COMP367 Group Project Updated");
 	  
 
         Scanner scanner = new Scanner(System.in);
